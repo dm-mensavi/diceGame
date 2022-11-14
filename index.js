372add05a65858f57b35173a313fc4c8c0c4d5a0
Mensavi David Mawuli <mensavidavid@gmail.com>
2022-11-14 04:51:16 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// document.querySelector(".play").setAttribute("onclick", this.innerHTML = startGame());
+
+function startGame (){
+
+    var randomNumber1 = Math.floor(Math.random()*6);
+    var randomNumber2 = Math.floor(Math.random()*6);
+
+    const imagesArray = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
+
+    document.querySelector(".img1").setAttribute("src",imagesArray[randomNumber1]);
+
+
+    document.querySelector(".img2").setAttribute("src",imagesArray[randomNumber2]);
+
+    if (randomNumber1 > randomNumber2)
+        document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+
+    else if (randomNumber1 < randomNumber2)
+        document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+
+    else{
+        document.querySelector("h1").innerHTML = "Draw!";
+        // startGame();
+    }
+    return "Roll Again";
+}
